2445a1cb2de9a74243d36d13b5601ec03d780428
Kingson Yang <kingsony1025@gmail.com>
2023-12-12 13:28:44 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -23,14 +23,14 @@ const App = () => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundImage: `url(${gif})`, backgroundRepeat: 'no-repeat', backgroundSize: '100%'}}>
       <ReactCardFlip isFlipped={click} flipDirection="horizontal">
-        <Box onClick={handleClick} sx={{ display: 'flex', justifyContent: 'center', width: '450px', aspectRatio: '7/11', backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', cursor: 'pointer', boxShadow: '5px 0px 5px 0px rgba(0,0,0,0.2)' }}>
+        <Box onClick={handleClick} sx={{ display: 'flex', justifyContent: 'center', width: {xs: '300px', sm: '450px'}, aspectRatio: '7/11', backgroundImage: `url(${img})`, backgroundRepeat: 'no-repeat', cursor: 'pointer', boxShadow: '5px 0px 5px 0px rgba(0,0,0,0.2)' }}>
           <Stack>
-            <Typography sx={{ fontFamily: 'Pinyon Script', fontSize: '50px', marginY: '50px' }}>Merry Christmas</Typography>
+            <Typography sx={{ textAlign: 'center', fontFamily: 'Pinyon Script', fontSize: {xs: '40px', sm: '50px'}, marginY: {xs: '60px', sm: '50px'} }}>Merry Christmas</Typography>
             <img src={milk} style={{ margin: '0 50px 0 50px', width: '200px' }}></img>
           </Stack>
         </Box>
-        <Box onClick={handleClick} sx={{ display: 'flex', background: 'white', alignItems: 'center', justifyContent: 'center', width: '450px', aspectRatio: '7/11', cursor: 'pointer', boxShadow: '5px 0px 5px 0px rgba(0,0,0,0.2)' }}>
-          <TypeAnimation style={{fontSize: '20px', whiteSpace: 'pre-line'}} speed={40} sequence={[`Your smile, a radiant sun that lights my day\nOur hearts entwined, a love that will never sway\nUnderneath the stars, our dreams begin to play\nReveling in the magic, in your arms, I'll stay\n\nMoments with you, a treasure beyond compare\nOverflowing joy, a bond that's truly rare\nMoments of laughter, moments we both share\n\n\nFrom Pookie <3`]} omitDeletionAnimation={true} wrapper="span"></TypeAnimation>
+        <Box onClick={handleClick} sx={{ display: 'flex', padding: '10px', background: 'white', alignItems: 'center', justifyContent: 'center', width: {xs: '300px', sm: '450px'}, aspectRatio: '7/11', cursor: 'pointer', boxShadow: '5px 0px 5px 0px rgba(0,0,0,0.2)' }}>
+          <TypeAnimation className={'type'} style={{whiteSpace: 'pre-line'}} speed={40} sequence={[`Your smile, a radiant sun that lights my day\nOur hearts entwined, a love that will never sway\nUnderneath the stars, our dreams begin to play\nReveling in the magic, in your arms, I'll stay\n\nMoments with you, a treasure beyond compare\nOverflowing joy, a bond that's truly rare\nMoments of laughter, moments we both share\n\n\nFrom Pookie <3`]} omitDeletionAnimation={true} wrapper="span"></TypeAnimation>
         </Box>
       </ReactCardFlip>
     </Box>
